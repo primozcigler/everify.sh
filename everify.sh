440095ffd91c21a974cc135228b3a15367e20744
Primoz Cigler <primoz@proteusnet.com>
2014-06-30 10:29:48 +0200

    Added the user agent header to the curl request. Suggested by dtbaker TF author.

--- everify.sh
+++ everify.sh
@@ -8,7 +8,7 @@ APIKEY='your_envato_api_key'
 read -p 'Enter purchase code: ' purchase_code
 
 # main part, here we get the response from the Envato APIs using curl library
-response=`curl -s http://marketplace.envato.com/api/edge/$USERNAME/$APIKEY/verify-purchase:$purchase_code.json`
+response=`curl --user-agent 'everify.sh by ProteusThemes' -s http://marketplace.envato.com/api/edge/$USERNAME/$APIKEY/verify-purchase:$purchase_code.json`
 
 # pipelining to the python tool is just to prettify the output. You can remove the pipeline | and
 # everything after it if you don't need the prettyfied format
